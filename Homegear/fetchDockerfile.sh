93eb697cb4ea5e9e50fcb624d216a2d7bb6a3859
Sven Igl <shogoki@gamersvillage.de>
2020-05-01 18:08:13 +0200

    add start.sh rom original Dockerfile

--- Homegear/fetchDockerfile.sh
+++ Homegear/fetchDockerfile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/Homegear/Homegear-Docker/master/stable/Dockerfile --output Dockerfile
+curl https://raw.githubusercontent.com/Homegear/Homegear-Docker/master/stable/start.sh --output start.sh
 # remove VOLUME and ENTRYPOINT declarations. Need to use tee as usual redirection did not work
 grep -v VOLUME Dockerfile | grep -v ENTRYPOINT  | tee Dockerfile > /dev/null
 #add custom entrypoint
